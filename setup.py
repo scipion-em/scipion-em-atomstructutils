6a95e5ed290795a5cfcd2b70a9b0f6c624b86ae6
marta <mmmtnez@cnb.csic.es>
2019-04-22 12:38:59 +0200

    new realease version

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='scipion-em-atomstructutils',
-    version='1.0.5b',
+    version='1.0.6b',
     description='A Scipion plugin to manipulate atomic structure files (PDB/MMCIF)',
     long_description=long_description,
     url='https://github.com/scipion-em/scipion-em-atomstructutils',
